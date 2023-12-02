4441e1cf6fa388b6d254288b03d74e5bbb29ae5d
PedroUnipam <130712531+PedroUnipam@users.noreply.github.com>
2023-12-02 15:51:32 -0300

    finaliza resolução parte 1 dia 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -3,9 +3,30 @@ import { getInput, writeAnswer } from "../helpers";
 export async function day1() {
     const input = await getInput(__dirname)
 
-    console.log(input)
+    const lines = input.split('\r\n')
 
-    const answer = 'your answer here'
+    const twoDigitNumbers: number[] = lines.map((line) => {
+        const chars = line.split('')
+        const numericChars = chars.filter((char) => {
+            const numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+            return numerals.includes(char)
+        })
+
+        const firstNumber = numericChars.at(0) as string
+        const lastNumber = numericChars.at(-1) as string
+
+        const twoDigitNumberAsString = firstNumber.concat(lastNumber)
+
+        const twoDigitNumber = +twoDigitNumberAsString
+
+        return twoDigitNumber
+    })
+
+    let sum = 0
+
+    twoDigitNumbers.forEach((twoDigitNumber) => sum += twoDigitNumber) 
+
+    const answer = sum.toString()
 
     await writeAnswer(__dirname, answer)
 }
